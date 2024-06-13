22fcfbe19d6ea1b7e26527b74acc7dee5a7a9e2d
Olejechka <144726472+Olejechka@users.noreply.github.com>
2024-06-13 14:49:15 +0300

    Add files via upload

---
+++ 6/1.pas
@@ -0,0 +1,34 @@
+﻿uses GraphABC,ml2;
+var
+  (m, wx, wy, g):= (100.0, 300.0, 300.0, 1.0);
+
+procedure KeyDown(k: integer);
+begin
+  case k of
+    VK_Down: wy -= 10.0;
+    VK_Up: wy += 10.0;
+    VK_Left: wx += 10.0;
+    VK_Right: wx -= 10.0;
+    VK_Escape: halt(1); //выход
+    VK_A: if m < 500.0 then m += 10.0;
+    VK_Z: if m > 20.0 then m -= 10.0;
+    VK_S: if g < 8.0 then g += 1.0;
+    VK_X: if g > 0.0 then g -= 1.0;
+  end;
+  Window.Clear;
+  LockDrawing;
+  ris(wx - m, wy, wx + m, wy, g);
+  Redraw;
+end;
+begin
+  m := 100.0;
+  wx := 300.0;
+  wy := 300.0;
+  g := 1.0;
+  SetWindowCaption('Фракталы: Снежинка Коха');
+  SetWindowSize(700, 512);
+  OnKeyDown := KeyDown;
+  LockDrawing;
+  ris(wx - m, wy, wx + m, wy, g);
+  Redraw;
+end.
---
+++ 6/ml2.pas
@@ -0,0 +1,26 @@
+﻿unit ml2;
+interface
+procedure ris(x1, y1, x2, y2, k: real);
+implementation
+uses
+  GraphABC;
+procedure ris(x1, y1, x2, y2, k: real);
+begin
+  if k > 0 then
+  begin
+    var xn := (2 * x1 + x2) / 3;
+    var yn := (2 * y1 + y2) / 3;
+    var xm := (x1 + 2 * x2) / 3;
+    var ym := (y1 + 2 * y2) / 3;
+    var xp := (x1 + x2) / 2 + (y2 - y1) / 3;
+    var yp := (y1 + y2) / 2 - (x2 - x1) / 3;
+
+    ris(x1, y1, xn, yn, k - 1);
+    ris(xn, yn, xp, yp, k - 1);
+    ris(xp, yp, xm, ym, k - 1);
+    ris(xm, ym, x2, y2, k - 1);
+  end
+  else
+    Line(Round(x1), Round(y1), Round(x2), Round(y2));
+end;
+end.
